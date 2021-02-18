51835e17346ac1769f2bf375c10577f1512886b1
VetalosUA-KR <vetal925@gmail.com>
2021-02-18 16:11:16 +0200

    add ViewModel

--- app/src/main/java/com/vitalii/architectureexample/MainActivity.java
+++ app/src/main/java/com/vitalii/architectureexample/MainActivity.java
@@ -1,17 +1,40 @@
 package com.vitalii.architectureexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 //@Vitalii Voitenko
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        noteViewModel = new ViewModelProvider
+                .AndroidViewModelFactory(getApplication())
+                .create(NoteViewModel.class);
 
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                //update RecyclerView
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
+
+
+
+
+
+
